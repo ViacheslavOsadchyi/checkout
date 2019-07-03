fa35a83ca8e9858b72c96e557e0687e0b8148170
Серега Мост <slava.osadchyi.dev@gmail.com>
2019-07-03 11:34:58 +0300

    Updates

---
+++ precache-manifest.10b999f0c406f671658ec20fe7d9e525.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "3ba42cbbc2b0872db6d7",
+    "url": "/ViacheslavOsadchyi/static/js/runtime~main.4be5394f.js"
+  },
+  {
+    "revision": "1b5adc92c798e1f610bb",
+    "url": "/ViacheslavOsadchyi/static/js/main.e073e5ba.chunk.js"
+  },
+  {
+    "revision": "dd583b54262aaf9ea4bb",
+    "url": "/ViacheslavOsadchyi/static/js/2.77017383.chunk.js"
+  },
+  {
+    "revision": "1b5adc92c798e1f610bb",
+    "url": "/ViacheslavOsadchyi/static/css/main.a81506b0.chunk.css"
+  },
+  {
+    "revision": "853e8f6743beee01665ac3d95e28ab75",
+    "url": "/ViacheslavOsadchyi/index.html"
+  }
+];
